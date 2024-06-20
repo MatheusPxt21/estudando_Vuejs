1e8500b0a0642ff5951d2e6d2acd593e5bbdf1bd
Matheus Peixoto <mateusnascimentopxt@gmail.com>
2024-06-20 17:30:29 -0300

    aula 20/06/2024

---
+++ estudando-vue/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte Seu Hamburguer</h1>
+    <BurgerForm />
+  </div>
+</template>
+
+<script>
+import Banner from '../components/Banner.vue'
+import BurgerForm from '../components/BurgerForm'
+
+export default {
+  name: "Home",
+  components: {
+    Banner,
+    BurgerForm
+  }
+}
+</script>
