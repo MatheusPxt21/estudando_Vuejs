13e226ef0fd9ac415af6396701bd6722d474c2be
Matheus Peixoto <mateusnascimentopxt@gmail.com>
2024-09-15 14:33:51 -0300

    inicio segundo projeto em vue para estudo

---
+++ segundo-projeto/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <HeaderComponent>
+    </HeaderComponent>
+  </div>
+</template>
+
+<script>
+
+import './styles/golbal.css'
+import HeaderComponent from './components/HeaderComponent.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
